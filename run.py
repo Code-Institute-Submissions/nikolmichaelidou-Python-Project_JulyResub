331ac5a23cbd198798a5aa52fd8d73a19b0e1a2b
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-02-12 13:28:35 +0000

    Added function that will show the game board

--- run.py
+++ run.py
@@ -5,9 +5,27 @@ letters_to_numbers = {'A': 0, 'B': 1, 'C': 2,
                       'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
-def board():
+def print_board(borad):
+    print('A B C D E F G H')
+    print("===============")
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
+
+
+def create_ships():
     pass
 
 
 def get_ship_location():
     pass
+
+
+def count_hit_ships():
+    pass
+
+
+create_ships()
+turns = 10
+while turns > 0:
