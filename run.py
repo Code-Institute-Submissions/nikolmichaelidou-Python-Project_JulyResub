6052470a5e655be37aca959e197ce5a08da1f355
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-02-13 21:50:38 +0000

    Created a function to show the board

--- run.py
+++ run.py
@@ -2,3 +2,12 @@ board = []
 
 for x in range(8):
     board.append(["O"] * 8)
+
+
+def print_board(board):
+    for row in board:
+        print((" ").join(row))
+
+
+print("Let's play Battleship!")
+print_board(board)
